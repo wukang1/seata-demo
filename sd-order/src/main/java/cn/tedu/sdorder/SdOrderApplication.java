940a48666af099b5c3fa692b5cb3cf37ceecbe40
benwang6 <45354575+benwang6@users.noreply.github.com>
2020-06-02 22:54:35 +0800

    订单服务调用库存和账户服务完成

--- sd-order/src/main/java/cn/tedu/sdorder/SdOrderApplication.java
+++ sd-order/src/main/java/cn/tedu/sdorder/SdOrderApplication.java
@@ -3,7 +3,9 @@ package cn.tedu.sdorder;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 @MapperScan("cn.tedu.sdorder.mapper")
 public class SdOrderApplication {
--- sd-order/src/main/java/cn/tedu/sdorder/service/OrderServiceImpl.java
+++ sd-order/src/main/java/cn/tedu/sdorder/service/OrderServiceImpl.java
@@ -1,6 +1,8 @@
 package cn.tedu.sdorder.service;
 
 import cn.tedu.sdorder.entity.Order;
+import cn.tedu.sdorder.feign.AccountClient;
+import cn.tedu.sdorder.feign.StorageClient;
 import cn.tedu.sdorder.mapper.OrderMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,11 +11,20 @@ import org.springframework.stereotype.Service;
 public class OrderServiceImpl implements OrderService {
     @Autowired
     private OrderMapper orderMapper;
+    @Autowired
+    private AccountClient accountClient;
+    @Autowired
+    private StorageClient storageClient;
 
     @Override
     public void create(Order order) {
         orderMapper.create(order);
-        //TODO: 修改库存
-        //TODO: 缓存
+
+        // 修改库存
+        storageClient.decrease(order.getProductId(), order.getCount());
+
+        // 修改账户余额
+        accountClient.decrease(order.getUserId(), order.getMoney());
+
     }
 }
